b9f151bf655dc574d386a3527e7911ee03bf969f
JeffBenson <68038932+JeffBenson@users.noreply.github.com>
2020-10-10 11:57:29 -0400

    Rot13 Algorithm Practice

---
+++ ROT13_Practice/Program.cs
@@ -0,0 +1,45 @@
+﻿//Developer: Jeff Benson
+//ROT13 Algorithm inspired by tutorial from dotnetpearls.com/rot13
+
+using System;
+
+namespace Rot13_Cipher_Practice
+{
+    class Program
+    {
+        static string Rot13(string input)
+        {
+            input = input.ToLower();
+            char[] array = input.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = (int)array[i];
+                if (number >= 'a' && number <= 'z')
+                {
+                    if (number > 'm')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                array[i] = (char)number;
+            }
+            return new string(array);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your phrase to be encoded: ");
+            string phrase = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Cipher text: " + Rot13(phrase));
+            Console.ReadKey();
+        }
+    }
+}
